f17d3d60e92aee839bbad250cd94a1eea5176533
ankmay0 <153919929+ankmay0@users.noreply.github.com>
2025-06-13 23:12:52 +0530

    Update DummyData.js

--- teamManager1/my-react-app/public/DummyData.js
+++ teamManager1/my-react-app/public/DummyData.js
@@ -1,7 +1,8 @@
+//team Id  -> use it in fileHnadler 
 const DummyData = [
-  { srcId: "101", srcName: "Neelmani", targetId: "102", targetName: "Raj" },
-  { srcId: "201", srcName: "Neelmani", targetId: "202", targetName: "Shankan" },
-  { srcId: "301", srcName: "Mayank", targetId: "302", targetName: "Rohit" },
+  { teamId:"team01" , srcId: "101", srcName: "Neelmani", targetId: "102", targetName: "Raj" },
+  { teamId:"team02" ,srcId: "201", srcName: "Neelmani", targetId: "202", targetName: "Shankan" },
+  { teamId:"team03" ,srcId: "301", srcName: "Mayank", targetId: "302", targetName: "Rohit" },
 ];
 
 export default DummyData;
